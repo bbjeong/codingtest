913724b45cc700e3b909c2f5686cb06496cabade
Bobae Jeong <bbjeong1121@gmail.com>
2024-06-10 11:25:46 +0900

    [level 1] Title: 폰켓몬, Time: 10.12 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,13 +3,9 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
-
-        int max = nums.length / 2;
-        if (set.size() > max) {
-            return max;
-        } else {
-            return set.size();
-        }
+        return Arrays.stream(nums).boxed().collect(
+                Collectors.collectingAndThen(
+                        Collectors.toSet(), resultSet -> Math.min(resultSet.size(), nums.length / 2)
+                ));
     }
 }
