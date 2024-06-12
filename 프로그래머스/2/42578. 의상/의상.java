345f876ddb0932a89236981952c1667de534ab24
Bobae Jeong <bbjeong1121@gmail.com>
2024-06-12 14:05:43 +0900

    [level 2] Title: 의상, Time: 6.67 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,13 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(String[][] clothes) {
         
-        Map<String, Integer> answers = new HashMap<>();
-        for (String[] c : clothes) {
-            answers.compute(c[1], (k, v) -> v == null ? 1 : v + 1);
-        }
-
-        int answer = 1;
-        List<Integer> list = new ArrayList<>(answers.values());
-        for (Integer i : list) {
-            answer *= (i + 1); // 경우의 수 + 아무것도 선택안한 케이스(+1)
-        }
-
-        return answer - 1; // 모두 선택 안한 케이스 제외
+        return Arrays.stream(clothes)
+                .collect(Collectors.groupingBy(c -> c[1], Collectors.counting()))
+                .values()
+                .stream().reduce(1L, (a, b) -> a * (b + 1)) // 경우의 수 + 아무것도 선택안한 케이스(+1)
+                .intValue() - 1; // 모두 선택 안한 케이스 제외
     }
 }
